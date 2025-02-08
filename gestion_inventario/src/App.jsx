d6daaa525ee9cb21d844c67d802813e52ff297b2
David Gil <dgilr@unal.edu.co>
2025-02-08 16:09:23 -0500

    cambio a js

---
+++ gestion_inventario/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div className=" bg-gray-100">
+        <h1 className=" text-3xl text-bold text-green-400">Hola</h1>
+        <p className="  "></p>
+      </div>
+    </>
+  )
+}
+
+export default App
